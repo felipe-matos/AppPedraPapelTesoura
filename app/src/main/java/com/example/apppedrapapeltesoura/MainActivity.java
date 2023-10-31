d34cfd4cabb581a5eef031cb690d24c2bd3729a1
Felipe Matos <felipematosapps@gmail.com>
2023-10-31 19:57:42 -0300

    segundo commit adicionado métodos na MainActivity.

--- app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.apppedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void selecionarPedra(View view){
+        verificarGanhador("Pedra");
+
+    }
+    public void selecionarPapel(View view){
+        verificarGanhador("Papel");
+
+    }
+    public void selecionarTesoura(View view){
+        verificarGanhador("Tesoura");
+
+    }
+
+    private void verificarGanhador(String escolhaUsuario){
+        System.out.println("Item clicado" + escolhaUsuario);
     }
 }
