39f1497467bccaaf7774274234c712c1b28a0545
Felipe Matos <felipematosapps@gmail.com>
2023-11-01 13:14:52 -0300

    quarto commit adicionado metodos na MainActivity

--- app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +30,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void verificarGanhador(String escolhaUsuario){
-        System.out.println("Item clicado" + escolhaUsuario);
+    private String gerarEscolhaAleatoria() {
+
+        String [] opcoes = {"pedra","papel","tesoura"};
+        int numeroAletario = new Random().nextInt(3);
+
+        ImageView imagemApp = findViewById(R.id.image_app);
+        String escolhaApp = opcoes[numeroAletario];
+
+        switch(escolhaApp){
+            case "pedra":
+                imagemApp.setImageResource(R.drawable.pedra);
+            break;
+            case "papel":
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+            case "Tesoura":
+                imagemApp.setImageResource(R.drawable.tesoura);
+                break;
+
+        }
+
+        return escolhaApp;
     }
+
+    private void verificarGanhador(String escolhaUsuario){
+
+        String escolhaApp = gerarEscolhaAleatoria();
+
+        //System.out.println("Item clicado" + escolhaUsuario);
+    }
+
+
 }
